e40501bc2e37489fed55e26e479eb18f9976cf55
Axel Pfeiffer <afpfeiffer@gmail.com>
2014-06-02 07:21:15 +0200

    add DataFragment to ActivityMain. month selection is preserved over configuration changes. data is re-loaded with each change. room for improovement.

--- app/src/main/java/com/pfeiffer/expenses/activity/ActivityMain.java
+++ app/src/main/java/com/pfeiffer/expenses/activity/ActivityMain.java
@@ -2,6 +2,8 @@ package com.pfeiffer.expenses.activity;
 
 import android.app.Activity;
 import android.app.DatePickerDialog;
+import android.app.Fragment;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -48,15 +50,11 @@ public class ActivityMain extends Activity {
 
     private RepositoryManager repositoryManager_;
     private ExpandableListView expandableListView_;
-    private ArrayList<HashMap<String, String>> mylist_title;
-    private ArrayList<ArrayList<HashMap<String, String>>> mylist;
     private SimpleExpandableListAdapter expListAdapter;
-    private HashMap<String, String> map1, map2;
-    Calendar myCalendar_;
-
-
     private TextView currentMonth_;
     private TextView totalExpenses_;
+
+    DataFragment dataFragment_;
 
 
     @Override
@@ -67,10 +65,17 @@ public class ActivityMain extends Activity {
         currentMonth_ = (TextView) findViewById(R.id.tvCurrentMonth);
         totalExpenses_ = (TextView) findViewById(R.id.tvTotalExpenses);
 
-        myCalendar_ = Calendar.getInstance();
-        myCalendar_.setTime(new Date(System.currentTimeMillis()));
-        updateCalendarTextview();
+        FragmentManager fragmentManager = getFragmentManager();
+        dataFragment_ = (DataFragment) fragmentManager.findFragmentByTag("ActivityMain");
 
+        if (dataFragment_ == null) {
+            // add the fragment
+            dataFragment_ = new DataFragment();
+            fragmentManager.beginTransaction().add(dataFragment_, "ActivityMain").commit();
+
+            dataFragment_.resetCalendar(); 
+        }
+        loadData();
 
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
 
@@ -78,10 +83,10 @@ public class ActivityMain extends Activity {
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
                 // TODO Auto-generated method stub
-                myCalendar_.set(Calendar.YEAR, year);
-                myCalendar_.set(Calendar.MONTH, monthOfYear);
-                myCalendar_.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateCalendarTextview();
+                dataFragment_.getCalendar().set(Calendar.YEAR, year);
+                dataFragment_.getCalendar().set(Calendar.MONTH, monthOfYear);
+                dataFragment_.getCalendar().set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                loadData();
             }
 
         };
@@ -91,30 +96,28 @@ public class ActivityMain extends Activity {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                DatePickerDialog pickerDialog=new DatePickerDialog(ActivityMain.this, date, myCalendar_
-                        .get(Calendar.YEAR), myCalendar_.get(Calendar.MONTH),
-                        myCalendar_.get(Calendar.DAY_OF_MONTH)
+                DatePickerDialog pickerDialog = new DatePickerDialog(ActivityMain.this, date,
+                        dataFragment_.getCalendar().get(Calendar.YEAR),
+                        dataFragment_.getCalendar().get(Calendar.MONTH),
+                        dataFragment_.getCalendar().get(Calendar.DAY_OF_MONTH)
                 );
-                DatePicker picker= pickerDialog.getDatePicker();
+                DatePicker picker = pickerDialog.getDatePicker();
 
                 try {
                     Field f[] = picker.getClass().getDeclaredFields();
                     for (Field field : f) {
-                        if (field.getName().equals("mDayPicker")||field.getName().equals("mDaySpinner")) {
+                        if (field.getName().equals("mDayPicker") || field.getName().equals("mDaySpinner")) {
                             field.setAccessible(true);
                             Object dayPicker = new Object();
                             dayPicker = field.get(picker);
                             ((View) dayPicker).setVisibility(View.GONE);
                         }
                     }
-                }
-                catch (SecurityException e) {
+                } catch (SecurityException e) {
                     Log.d("ERROR", e.getMessage());
-                }
-                catch (IllegalArgumentException e) {
+                } catch (IllegalArgumentException e) {
                     Log.d("ERROR", e.getMessage());
-                }
-                catch (IllegalAccessException e) {
+                } catch (IllegalAccessException e) {
                     Log.d("ERROR", e.getMessage());
                 }
 
@@ -123,87 +126,76 @@ public class ActivityMain extends Activity {
         });
 
 
-
-
         registerForContextMenu(expandableListView_);
-        showActivity();
-    }
-
-
-    private void updateCalendarTextview() {
-        Log.d(logTag_, myCalendar_.getTime().toString());
-        showActivity();
-
-        String myFormat = "MMMM yyyy"; //In which you need put here
-        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.GERMAN);
-
-        currentMonth_.setText(sdf.format(myCalendar_.getTime()));
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         repositoryManager_.close();
     }
 
-
-
-    private void showActivity() {
-        mylist = new ArrayList<ArrayList<HashMap<String, String>>>();
-        mylist_title = new ArrayList<HashMap<String, String>>();
+    private void loadData() {
+        dataFragment_.setMylist(new ArrayList<ArrayList<HashMap<String, String>>>());
+        dataFragment_.setMylist_title(new ArrayList<HashMap<String, String>>());
 
         // get all Purchases
         repositoryManager_ = new RepositoryManager(this);
         repositoryManager_.open();
 
         List<Purchase> purchasesCurrentMonth = repositoryManager_.getAllPurchasesForDateRange(
-                Translation.getFirstDateOfMonth(myCalendar_.getTime()),
-                Translation.getFirstDateOfNextMonth(myCalendar_.getTime()));
+                Translation.getFirstDateOfMonth(dataFragment_.getCalendar().getTime()),
+                Translation.getFirstDateOfNextMonth(dataFragment_.getCalendar().getTime()));
 
         DataAnalysis dataAnalysis = new DataAnalysis(repositoryManager_, purchasesCurrentMonth);
 
-//        currentMonth_.setText(Translation.getMonthString(rightNow));
-        Money totalExpenses = dataAnalysis.getExpensesPerMonth(myCalendar_.getTime());
+        String myFormat = "MMMM yyyy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.GERMAN);
+
+        currentMonth_.setText(sdf.format(dataFragment_.getCalendar().getTime()));
+
+        Money totalExpenses = dataAnalysis.getExpensesPerMonth(dataFragment_.getCalendar().getTime());
         totalExpenses_.setText(totalExpenses.getHumanReadableRepresentation());
 
         List<Map.Entry<EnumCategory, Money>> categoryToExpenses = dataAnalysis.getSortedCategoryToExpensesForYearAndMonth
-                (myCalendar_.getTime());
+                (dataFragment_.getCalendar().getTime());
 
         int len = categoryToExpenses.size();
         for (int i = 0; i < len; ++i) {
             Map.Entry<EnumCategory, Money> entry = categoryToExpenses.get(len - 1 - i);
             EnumCategory category = entry.getKey();
-            List<Purchase> purchases = dataAnalysis.getPurchasesForYearMonthAndCategory(myCalendar_.getTime(), category);
+            List<Purchase> purchases = dataAnalysis.getPurchasesForYearMonthAndCategory(
+                    dataFragment_.getCalendar().getTime(), category);
             if (purchases != null && !purchases.isEmpty()) {
                 ArrayList<HashMap<String, String>> secList = new ArrayList<HashMap<String, String>>();
 
-                map1 = new HashMap<String, String>();
+                dataFragment_.setMap1(new HashMap<String, String>());
                 int percentage = entry.getValue().percentage(totalExpenses);
-                map1.put("rowCategoryName", category.toString() + " (" + percentage + "%)");
-                map1.put("rowCategoryExpenses", entry.getValue().getHumanReadableRepresentation());
-                mylist_title.add(map1);
+                dataFragment_.getMap1().put("rowCategoryName", category.toString() + " (" + percentage + "%)");
+                dataFragment_.getMap1().put("rowCategoryExpenses", entry.getValue().getHumanReadableRepresentation());
+                dataFragment_.getMylist_title().add(dataFragment_.getMap1());
 
                 for (Purchase purchase : purchases) {
 
-                    map2 = new HashMap<String, String>();
-                    map2.put("purchaseId", String.valueOf(purchase.getId()));
-                    map2.put("rowDate", Translation.shortDate(purchase.getDate()));
-                    map2.put("rowName", ((purchase.getAmount() > 1) ? purchase.getAmount() + "x " : "")
+                    dataFragment_.setMap2(new HashMap<String, String>());
+                    dataFragment_.getMap2().put("purchaseId", String.valueOf(purchase.getId()));
+                    dataFragment_.getMap2().put("rowDate", Translation.shortDate(purchase.getDate()));
+                    dataFragment_.getMap2().put("rowName", ((purchase.getAmount() > 1) ? purchase.getAmount() + "x " : "")
                             + purchase.getProductName());
 
-                    map2.put("rowTotalPrice", purchase.getTotalHumanReadablePrice());
-                    secList.add(map2);
+                    dataFragment_.getMap2().put("rowTotalPrice", purchase.getTotalHumanReadablePrice());
+                    secList.add(dataFragment_.getMap2());
                 }
 
-                mylist.add(secList);
+                dataFragment_.getMylist().add(secList);
             }
         }
 
 
         try {
-            expListAdapter = new SimpleExpandableListAdapter(this, mylist_title, R.layout.group_row, new String[]{
+            expListAdapter = new SimpleExpandableListAdapter(this, dataFragment_.getMylist_title(), R.layout.group_row, new String[]{
                     "rowCategoryName", "rowCategoryExpenses"}, new int[]{R.id.rowCategoryName, R.id.rowCategoryExpenses},
-                    mylist, R.layout.row, new String[]{"rowDate", "rowName", "rowTotalPrice"}, new int[]{
+                    dataFragment_.getMylist(), R.layout.row, new String[]{"rowDate", "rowName", "rowTotalPrice"}, new int[]{
                     R.id.rowDate, R.id.rowName, R.id.rowTotalPrice}
             );
             expandableListView_.setAdapter(expListAdapter);
@@ -263,11 +255,11 @@ public class ActivityMain extends Activity {
         // Only create a context menu for child items
         if (type == ExpandableListView.PACKED_POSITION_TYPE_CHILD) {
             // Array created earlier when we built the expandable list
-            menu.setHeaderTitle(mylist.get(group).get(child).get("rowName"));
+            menu.setHeaderTitle(dataFragment_.getMylist().get(group).get(child).get("rowName"));
             menu.add(0, 0, 0, "Bearbeiten"); //TODO
             menu.add(0, 1, 0, "Löschen"); //TODO
             Log.d(logTag_, "ContextMenu created for item with id "
-                    + mylist.get(group).get(child).get("purchaseId"));
+                    + dataFragment_.getMylist().get(group).get(child).get("purchaseId"));
         }
     }
 
@@ -299,7 +291,7 @@ public class ActivityMain extends Activity {
     }
 
     private boolean editPurchase(int groupPos, int childPos) {
-        int purchaseId = Integer.parseInt(mylist.get(groupPos).get(childPos).get("purchaseId"));
+        int purchaseId = Integer.parseInt(dataFragment_.getMylist().get(groupPos).get(childPos).get("purchaseId"));
         Intent intent = new Intent(this, ActivityRecordPurchase.class);
         intent.putExtra(EXTRA_PURCHASE_ID, purchaseId);
         startActivity(intent);
@@ -311,11 +303,74 @@ public class ActivityMain extends Activity {
     private boolean deletePurchase(int groupPos, int childPos) {
 
         repositoryManager_.deletePurchase(Integer
-                .parseInt(mylist.get(groupPos).get(childPos).get("purchaseId")));
+                .parseInt(dataFragment_.getMylist().get(groupPos).get(childPos).get("purchaseId")));
 
         startActivity(new Intent(this, ActivityMain.class));
         finish();
 
         return true;
+    }
+
+    private class DataFragment extends Fragment {
+        Calendar calendar_;
+        private HashMap<String, String> map1, map2;
+        private ArrayList<HashMap<String, String>> mylist_title;
+        private ArrayList<ArrayList<HashMap<String, String>>> mylist;
+
+
+        // this method is only called once for this fragment
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            // retain this fragment
+            setRetainInstance(true);
+
+
+        }
+
+        public void resetCalendar() {
+            calendar_ = Calendar.getInstance();
+            calendar_.setTime(new Date(System.currentTimeMillis()));
+        }
+
+        public Calendar getCalendar() {
+            return calendar_;
+        }
+
+        public void setCalendar(Calendar calendar) {
+            this.calendar_ = calendar;
+        }
+
+        public HashMap<String, String> getMap1() {
+            return map1;
+        }
+
+        public void setMap1(HashMap<String, String> map1) {
+            this.map1 = map1;
+        }
+
+        public HashMap<String, String> getMap2() {
+            return map2;
+        }
+
+        public void setMap2(HashMap<String, String> map2) {
+            this.map2 = map2;
+        }
+
+        public ArrayList<HashMap<String, String>> getMylist_title() {
+            return mylist_title;
+        }
+
+        public void setMylist_title(ArrayList<HashMap<String, String>> mylist_title) {
+            this.mylist_title = mylist_title;
+        }
+
+        public ArrayList<ArrayList<HashMap<String, String>>> getMylist() {
+            return mylist;
+        }
+
+        public void setMylist(ArrayList<ArrayList<HashMap<String, String>>> mylist) {
+            this.mylist = mylist;
+        }
     }
 }
